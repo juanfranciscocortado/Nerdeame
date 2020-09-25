9b8215eb5f4f44626913384b0350aa694dba61dd
juanfranciscocortado <juanfrancisco.cortado@gmail.com>
2020-09-25 14:47:48 +0200

    Incorporación del Toolbar vacio en mi pantalla de inicio

--- app/src/main/java/com/example/nerdeame/MainActivity.kt
+++ app/src/main/java/com/example/nerdeame/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.nerdeame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //Aqui es donde tendremos lo referente a la funcionalidad y visualizacion de nuestro menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        //Esto es lo que hace que se vea nuestro barra del toolbar
+        menuInflater.inflate(R.menu.menu_controler_extension, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
